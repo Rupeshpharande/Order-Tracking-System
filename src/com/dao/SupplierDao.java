121a5d3b6bcc72082ff7b3ddbf69b325a4f663ae
Rupeshpharande <pharanderupesh@gmail.com>
2025-03-29 14:15:35 +0530

    first commit

---
+++ src/com/dao/SupplierDao.java
@@ -0,0 +1,12 @@
+package com.dao;
+
+import com.entity.Supplier;
+
+public interface SupplierDao {
+
+	void addSupplier(Supplier s);
+	boolean deleteSupplier(int sid);
+	void viewAllSupplier();
+	void viewSupplierById(int sid);
+	void displaynewsup(Supplier s);
+}
